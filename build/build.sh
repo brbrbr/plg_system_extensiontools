c5de0b9cc71ada1fdc341f10acb63ba55945eb13
bram <github@brambring.nl>
2024-06-12 17:43:02 +0200

    update description

--- build/build.sh
+++ build/build.sh
@@ -12,7 +12,7 @@ sed -i -e "s/$PATTERN/$VERSION/g" 44.sh
 zip -r $EXTENSION_ZIP_FILENAME language/ "$EXTENSION_ELEMENT.xml"  services/ forms/ src/ script.php --quiet
 
 SHA512=$(sha512sum $EXTENSION_ZIP_FILENAME | awk '{print $1}')
-sed -i -e "s/\(<sha512>\).*\(<\/sha512>\)/<sha512>$SHA512<\/sha512>/g"  \
+sed -i -e "0,/\(<sha512>\).*\(<\/sha512>\)/s//<sha512>$SHA512<\/sha512>/"  \
  -e "s/$PATTERN/$VERSION/g" \
   update.xml
 echo 'package and update server ready'
