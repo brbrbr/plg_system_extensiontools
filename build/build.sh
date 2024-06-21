9e7c4a3fbc2753dadf5bb037323606bed68bf2d3
bram <github@brambring.nl>
2024-06-21 11:45:00 +0200

    prepare release Extension Tools 5.1.6

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 EXTENSION_ZIP_FILENAME="build/plg_system_extensiontools.zip"
 EXTENSION_ELEMENT="extensiontools"
-PATTERN="4\.4\.[0-9]\+"
-VERSION="4.4.6"
+PATTERN="5\.1\.[0-9]\+"
+VERSION="5.1.6"
 if [ ! -f "$EXTENSION_ELEMENT.xml" ]; then cd ..; fi
 if [ -f "$EXTENSION_ZIP_FILENAME" ]; then rm $EXTENSION_ZIP_FILENAME; fi
 
