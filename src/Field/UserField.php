eb0f5a31b191a162fdbfbee1153ab1d001786bad
bram <github@brambring.nl>
2024-06-22 12:51:33 +0200

    prepare release System - Extension Tools 4.4.8

--- src/Field/UserField.php
+++ src/Field/UserField.php
@@ -3,8 +3,8 @@
 /**
  * Joomla! Content Management System
  *
- * @copyright  (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright 2024 Bram Brambring (https://brambring.nl)
+ * @license   GNU General Public License version 3 or later;
  */
 
 namespace Brambring\Plugin\System\Extensiontools\Field;
